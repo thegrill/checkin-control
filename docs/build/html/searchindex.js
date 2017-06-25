1259d6d7150f0a03e77e6d1b99e7bbd38681191c
chrizzftd <chris.gfz@gmail.com>
2017-06-25 23:12:55 +1000

    added docs (#1)
    docs html gh pages

---
+++ docs/build/html/searchindex.js
@@ -0,0 +1,1 @@
+Search.setIndex({docnames:["index"],envversion:53,filenames:["index.rst"],objects:{},objnames:{},objtypes:{},terms:{index:0,modul:0,page:0,search:0},titles:["Welcome to checkin-control\u2019s documentation!"],titleterms:{checkin:0,control:0,document:0,indic:0,overview:0,tabl:0,welcom:0}})
